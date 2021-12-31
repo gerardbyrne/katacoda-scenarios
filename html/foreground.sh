3a45dd6e5ab91bb7954ea7c9c5543b997de5b29b
Gerry Byrne <gerardbyrne@lineone.net>
2021-12-31 17:48:35 +0000

    Remove foreground.sh script to load HTML Preview extension

--- html/foreground.sh
+++ html/foreground.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-cd /tmp && apt-get update -y && apt install libarchive-tools -y # install bsdtar
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/humao/vsextensions/rest-client/0.24.3/vspackage | bsdtar -xvf - extension
-mv extension /opt/.katacodacode/extensions/tht13.html-preview-vscode
+printf 'This is a bash shell script.\n\n'
+printf 'Any commands you put in here will be executed at runtime.\n\n'
+printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
+printf 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
+printf 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
