2138752e61b7a060d09eefb44afda93986210662
Gerry Byrne <gerardbyrne@lineone.net>
2022-01-05 19:50:31 +0000

    Added foreground bash command for HTML Preview

--- html/foreground.sh
+++ html/foreground.sh
@@ -5,4 +5,4 @@ printf 'Any commands you put in here will be executed at runtime.\n\n'
 printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
 printf 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
 printf 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
-sudo snap --install-extension yummygum.city-lights-theme
+sudo snap install-extension yummygum.city-lights-theme
